f07e7e4c904a9e165c2303d69caf0acb9a7845b1
crolbar <crolbar@crolbar.com>
2024-07-27 07:43:48 +0300

    lang extensions in mod

--- lua/disc/init.lua
+++ lua/disc/init.lua
@@ -155,7 +155,7 @@ function disc:disconnect()
 end
 
 local function get_img_txt(filename, extension)
-    local extensions = { [".ino"] = "arduino", [".rs"] = "rust", [".c"] = "c", [".h"] = "c", [".cpp"] = "cpp", [".hpp"] = "cpp", [".cs"] = "c_sharp", [".css"] = "css", [".toml"] = "toml", [".go"] = "go", [".hs"] = "haskell", [".html"] = "html", [".java"] = "java", [".js"] = "javascript", [".json"] = "json", [".lua"] = "lua", [".md"] = "markdown", [".nix"] = "nix", [".ml"] = "ocaml", [".php"] = "php", [".py"] = "python", [".r"] = "r", [".rust"] = "rust", [".sass"] = "sass", [".sh"] = "shell", [".ts"] = "typescript", [".vim"] = "vim", [".wasm"] = "webassembly", [".zig"] = "zig", }
+    local extensions = require("disc.langs")
 
     local img_name = extensions[extension]
     local txt = nil
